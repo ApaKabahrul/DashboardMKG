8b2cdfb42383a3b2f6d2bb9cd14829d4382aaf7d
Mokalu <irfangerard44@gmail.com>
2019-03-26 14:59:07 +0700

    Membenarkan Capsa
    Capsa sudah bisa tampil dan digunakan namun tombol submit masih menggunakan bawaan dari capsa dan alert masih error

--- SourceCode/application/controllers/login.php
+++ SourceCode/application/controllers/login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class login extends CI_Controller {
 
 	public function __construct()
 	{
@@ -11,30 +11,60 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin/login');
+		//$this->load->view('admin/login');
+        $this->load->library('mathcaptcha');
+        $this->mathcaptcha->init();
+		$data['math_captcha_question'] = $this->mathcaptcha->get_question();
+
+        $this->form_validation->set_rules('math_captcha', 'Math CAPTCHA', 'required|callback__check_math_captcha');
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->load->view('admin/login', $data);
+        }
+        else
+        {
+            $this->load->view('admin/login', $data);
+        }
 	}
 
 	function aksi_login(){
+
+        $this->load->library('mathcaptcha');
+        $this->mathcaptcha->init();
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$where = array('username' => $username,
 					   'password' => md5($password));
 		$cek = $this->login_model->cek_login("admin",$where)->num_rows();
-		if($cek>0){
-			$data_session = array
-			('nama' => $username,
-			 'status' => "login" );
+        $str = $this->input->post('math_captcha');
 
-			$this->session->set_userdata($data_session);
-			redirect(base_url("admin"));
-		}
-		else {
-			echo "Pass Salah";
-		}
+        if ($this->mathcaptcha->check_answer($str))
+        {
+            if($cek>0){
+                $data_session = array
+                ('nama' => $username,
+                    'status' => "login" );
+
+                $this->session->set_userdata($data_session);
+                $a = true;
+                redirect(base_url("admin"));
+            }
+            else {
+                echo "<script type='text/javascript'>alert('Password Salah');</script>";
+                $a = false;
+            }
+        }else
+        {
+            echo "<script type='text/javascript'>alert('capsa salah');</script>";
+            $a = false;
+            //redirect(base_url("login"));
+        }
+
 	}
 
 	function logout(){
 		$this->session->sess_destroy();
-		redirect(base_url(''));
+		redirect(base_url('admin/login'));
 	}
 }
--- SourceCode/application/views/admin/login.php
+++ SourceCode/application/views/admin/login.php
@@ -17,10 +17,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			
 			<input type="text" name="username" class="form_login" placeholder="Username"/>
 			<input type="password" name="password" class="form_login" placeholder="Password"/>
- 			<label class="checkbox">
-          		<input type="checkbox" value="remember-me"> Remember me
-        	</label>
-			<input type="submit" class="tombol_login" value="Login"/>
+
+            <?php echo validation_errors(); ?>
+
+            <?php echo form_open(); ?>
+
+            <?php echo $math_captcha_question;?>
+
+            <?php echo $str = form_input('math_captcha');?>
+            <?php echo "<script type='text/javascript'>alert('$str');</script>"; ?>
+
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            <?php echo form_submit('submit', 'Submit'); ?>
+
+            <?php echo form_close();?>
+<!--            belum bisa dipake-->
+<!--			<input type="submit" class="tombol_login" value="Submit"/>-->
 		</form>
 	</div> 
 </body>
