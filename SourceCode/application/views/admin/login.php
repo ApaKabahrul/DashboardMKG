783ea89cc664eb54d77778256f49087f840df12d
ApaKabahrul <bahrul.faizi@gmail.com>
2019-03-26 20:09:13 +0700

    Pembaharuan UI

--- SourceCode/application/views/admin/login.php
+++ SourceCode/application/views/admin/login.php
@@ -10,14 +10,49 @@ $data = array(
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+	<title>DASHBOARD MKG</title>
+    <script src="<?php echo base_url()?>assets/js/jquery_004.js"></script> 
+    <script src="<?php echo base_url()?>assets/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/css.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/font-awesome.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/dataTables.css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/validationEngine.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/datepicker3.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/ladda-themeless.css">
+    <link rel="shortcut icon" href="<?php echo base_url()?>assets/img/logo%20itera%20oke.png">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/chosen.css">
+    
+  <style type="text/css"></style>
 </head>
-<body background="<?php echo base_url(); ?>assets/img/full-bloom.png">
-<h1>Sistem Dashboard MKG <br/> Institut Teknologi Sumatera</h1>
-
-	<div class="kotak_login">
-		<p class="tulisan_admin">Admin Area</p>
- 
+<div class="container base">
+  <div class="container">
+      <div class="row ">
+        <div class="col-md-1">
+            <a href="http://siakad.itera.ac.id/">
+              <img src="<?php echo base_url()?>assets/img/logo-itera.png" style="margin-bottom:10px; " width="70px">
+            </a>
+        </div>
+        <div class="col-md-5">
+          <h3>SISTEM DASHBOARD MKG</h3>
+          <p><em>"Institut Teknologi Sumatera"</em></p>
+        </div>
+      </div>
+  </div>
+</div>
+  <div class="container top">
+  <nav class="navbar navbar-inverse">  
+      <div class="navbar-header">
+      </div>
+  </nav>
+</div>
+<div class="container content">
+  <div class="col-md-12">
+    <div class="kotak_login">
+        <p class="form_title">Admin Area</p>
 		<form action="<?php echo base_url('login/aksi_login'); ?>" method="post">
             <?php echo validation_errors(); ?>
 
@@ -40,6 +75,10 @@ $data = array(
 <!--            belum bisa dipake-->
 <!--			<input type="submit" class="tombol_login" value="Submit"/>-->
 		</form>
-	</div> 
+
+    </div> 
 </body>
+  </div>
+</div>
+</div>
 </html>
