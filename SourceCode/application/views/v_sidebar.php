0e954260ef316890b35af0ca335d9b6d462aef85
meidiniadewi <45037640+meidiniadewi@users.noreply.github.com>
2019-04-06 20:59:15 +0700

    Update v_sidebar.php

--- SourceCode/application/views/v_sidebar.php
+++ SourceCode/application/views/v_sidebar.php
@@ -1,28 +1,25 @@
 <div class="container content">
-    <div class="row"><div class="col-md-3">
+    <div class="row">
+        <div class="col-md-3">
+            <?php if($this->session->userdata('nama')){ ?>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-header">Administrator</a>
+                    <a href="<?php echo base_url('admin');?>" class="list-group-item lima"><i class="fa fa-home tab10 lima" aria-hidden="true"></i>Upload</a>
+                </div>
+            <?php }else{
 
-  <div class="list-group">
-    <a href="http://siakad.itera.ac.id/mahasiswa" class="list-group-item active"><i class="fa fa-home tab10" aria-hidden="true"></i>Dashboard</a>
+            }?>
+            </ul>
 
-  </div>
-
-  <div class="list-group">
-    <a href="#" class="list-group-item list-group-item-header">Menu</a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Temperature</a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Sun Radiation </a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Relative Humidity</a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Wind Direction </a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Wind Speed </a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Pressure </a>
-    <a href="#" class="list-group-item ">
-      <i class="fa fa-list-ul tab10" aria-hidden="true"></i> Rain Rate </a>
-    
-  </div>
-
-  </div>
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-header">Menu</a>
+                <a href="<?php echo base_url('home');?>" class="list-group-item satu">
+                    <i class="fa fa-list-ul tab10 satu" aria-hidden="true"></i> Tahun</a>
+                <a href="#" class="list-group-item ">
+                    <i class="fa fa-list-ul tab10 dua" aria-hidden="true"></i> Bulan </a>
+                <a href="#" class="list-group-item ">
+                    <i class="fa fa-list-ul tab10 tiga" aria-hidden="true"></i> Minggu </a>
+                <a href="#" class="list-group-item ">
+                    <i class="fa fa-list-ul tab10 empat" aria-hidden="true"></i> Hari </a>
+            </div>
+        </div>
