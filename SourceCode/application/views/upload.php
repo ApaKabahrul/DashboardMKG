6b2d4b2f04261f5c26158690b0d729433570d1f2
Fristy Riani Puspitasari <38092181+fristyriani@users.noreply.github.com>
2019-04-08 00:16:29 +0700

    Update view upload

--- SourceCode/application/views/upload.php
+++ SourceCode/application/views/upload.php
@@ -1,16 +1,16 @@
 <body>
-  <center><h1>UPLOAD FILE ADMIN</h1></center>
- <br></br>
 
+	<div class="col-md-3"></div>
+	<div class="col-md-9">
+		<div class="container content">
+			<center><h1>UPLOAD FILE ADMIN</h1></center>
+ 			<br></br>
+  		<div class="card" style="margin-left: 250px; margin-right: 250px; border-style: solid; padding: 15px;">
+  		
+    <div class="form-group">
+      <input type="file" class="form-control-file border" name="file">
+    </div>
+    <button type="button" class="btn btn-warning" style="margin-left: 100px;">Submit</button>
+ 	</div>\
 
-  <div class="row">
-  <div class="col-md-6">
-  <div class="container">
-  <div class="card" style="width:350px; margin-left: 400px;">
-  <input type="file" name="berkas" />
- 
-  <br /><br />
- 
-  <input type="submit" value="upload" />
- 
 </body>
