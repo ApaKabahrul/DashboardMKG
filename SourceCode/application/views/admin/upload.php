b8a8d9702433d0af01bb6f1fb880698f9e55ec8b
Rizki Juliansyah <37908533+rizkijuliansyaham@users.noreply.github.com>
2019-04-08 13:57:53 +0700

    Add files via upload

--- SourceCode/application/views/admin/upload.php
+++ SourceCode/application/views/admin/upload.php
@@ -1,26 +1,17 @@
-<?php
-if($this->session->userdata('nama')){
-
-}else{
-    
-}?>
-<script src="<?php echo base_url()?>assets/js/uploadcsv.js"></script>
 <body>
-  <center><h1>UPLOAD FILE ADMIN</h1></center>
- <br></br>
 
+	
+	<div class="col-md-9">
+		<div class="container content">
+			<center><h1>UPLOAD FILE ADMIN</h1></center>
+             <br></br>
+             
+  		<div class="card" style="margin-left: 250px; margin-right: 250px; border-style: solid; padding: 15px;">
+  		
+    <div class="form-group">
+      <input type="file" class="form-control-file border" name="file">
+    </div>
+    <button type="button" class="btn btn-warning" style="margin-left: 100px;">Submit</button>
+ 	</div>\
 
-  <div class="row">
-  <div class="col-md-6">
-  <div class="container">
-      <form method="post" id="import_csv" enctype="multipart/form-data">
-          <div class="card" style="width:350px; margin-left: 400px;">
-              <div class="form-group">
-                  <label>Select CSV File</label>
-                    <input type="file" name="csv_file" id="csv_file" required accept=".csv" />
-              </div>
-          <br /><br />
-
-          <input type="submit" value="upload" id="import_csv_btn" />
-      </form>
 </body>
