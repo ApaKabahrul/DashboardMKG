204c3678f1d2fe29c46beff449abe3b9e0203073
Fristy Riani Puspitasari <38092181+fristyriani@users.noreply.github.com>
2019-04-23 07:12:56 +0700

    Update v_sidebar.php
    edit slide bar dan kasih icon

--- SourceCode/application/views/v_sidebar.php
+++ SourceCode/application/views/v_sidebar.php
@@ -4,7 +4,7 @@
             <?php if($this->session->userdata('nama')){ ?>
                 <div class="list-group">
                     <a href="#" class="list-group-item list-group-item-header">Administrator</a>
-                    <a href="<?php echo base_url('admin');?>" class="list-group-item lima"><i class="fa fa-home tab10 lima" aria-hidden="true"></i>Upload</a>
+                    <a href="<?php echo base_url('admin');?>" class="list-group-item lima"><i class="fa fa-upload tab10 lima" aria-hidden="true"></i>Upload</a>
                 </div>
             <?php }else{
 
@@ -13,13 +13,27 @@
 
             <div class="list-group">
                 <a href="#" class="list-group-item list-group-item-header">Menu</a>
-                <a href="<?php echo base_url('home');?>" class="list-group-item satu">
-                    <i class="fa fa-list-ul tab10 satu" aria-hidden="true"></i> Tahun</a>
-                <a href="#" class="list-group-item ">
-                    <i class="fa fa-list-ul tab10 dua" aria-hidden="true"></i> Bulan </a>
-                <a href="#" class="list-group-item ">
-                    <i class="fa fa-list-ul tab10 tiga" aria-hidden="true"></i> Minggu </a>
-                <a href="#" class="list-group-item ">
-                    <i class="fa fa-list-ul tab10 empat" aria-hidden="true"></i> Hari </a>
+
+                <a href="<?php echo base_url('Home');?>" class="list-group-item satu">
+                    <i class="fa fa-sun-o tab10" aria-hidden="true"></i>Panas Matahari</a>
+
+                <a href="<?php echo base_url('Home/Suhuview');?>" class="list-group-item dua">
+                    <i class="fa fa-thermometer-empty tab10 dua " aria-hidden="true"></i>&nbsp; Suhu</a>
+
+                <a href="<?php echo base_url('Home/Kelembabanview');?>" class="list-group-item tiga">
+                    <i class="fa fa-cloud tab10 " aria-hidden="true"></i> Kelembaban </a>
+
+                <a href="<?php echo base_url('Home/arahanginview');?>" class="list-group-item empat">
+                    <i class="fa fa-location-arrow tab10" aria-hidden="true"></i>&nbsp; Arah Angin </a>
+
+                <a href="<?php echo base_url('Home/kecepatananginview');?>" class="list-group-item enam">
+                    <i class="fa fa-tachometer tab10" aria-hidden="true"></i>&nbsp;Kecepatan Angin </a>
+
+                <a href="<?php echo base_url('Home/tekananview');?>" class="list-group-item tujuh">
+                    <i class="fa fa-thermometer-3 tab10" aria-hidden="true"></i>&nbsp; Tekanan Udara </a>
+
+                <a href="<?php echo base_url('Home/curahhujanview');?>" class="list-group-item delapan">
+                    <i class="fa fa-cloud tab10" aria-hidden="true"></i> Curah Hujan </a>
+
             </div>
         </div>
