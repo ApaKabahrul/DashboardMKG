209302ca74a66d7ff882f1bce7b370e40a5dee11
Mokalu <irfangerard44@gmail.com>
2019-05-05 13:51:19 +0700

    Update config
    update dilakukan agar aplikasi dapat di akses secara local dalam 1 jaringan dan menghapus koneksi terhadap database mysql

--- SourceCode/application/config/autoload.php
+++ SourceCode/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','form_validation');
+$autoload['libraries'] = array('session','form_validation','Mongo_db');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'captcha');
+$autoload['helper'] = array('url', 'captcha','form');
 
 /*
 | -------------------------------------------------------------------
