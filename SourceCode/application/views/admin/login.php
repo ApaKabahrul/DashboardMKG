2ed1a53ab1366c76ab776e5920adcfb0b6d92321
Mokalu <irfangerard44@gmail.com>
2019-05-05 12:55:37 +0700

    Update perubahan setelah testing
    Perubahan terjadi di tampilan halaman login dan upload

--- SourceCode/application/views/admin/login.php
+++ SourceCode/application/views/admin/login.php
@@ -1,12 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$data = array(
-    'name'        => 'math_captcha',
-    'value'          => '',
-    'class'       => 'form_login'
-);
 if($this->session->userdata('nama')){
-    redirect(base_url('admin/upload'));
+    redirect(base_url('/admin'));
 }else{
 
 }?>
@@ -26,14 +21,10 @@ if($this->session->userdata('nama')){
 			<input type="text" name="username" class="form_login" placeholder="Username"/>
 			<input type="password" name="password" class="form_login" placeholder="Password"/>
 
-            <?php echo $math_captcha_question;?>
+            <p id="capcha" v><?php echo $math_captcha_question;?></p>
+            <input type="text" name="math_captcha" class="form_login" placeholder="captcha" />
             <?php echo "<p></p>"?>
-            <?php echo $str = form_input($data); ?>
-<!--            <label class="checkbox">-->
-<!--                <input type="checkbox" value="remember-me"> Remember me-->
-<!--            </label>-->
             <?php echo form_submit('submit', 'Submit',"class='tombol_login '"); ?>
-
             <?php echo form_close();?>
 <!--            belum bisa dipake-->
 <!--			<input type="submit" class="tombol_login" value="Submit"/>-->
--- SourceCode/application/views/admin/upload.php
+++ SourceCode/application/views/admin/upload.php
@@ -1,16 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$data = array(
-    'name'        => 'math_captcha',
-    'value'          => '',
-    'class'       => 'form_login'
-);
-if($this->session->userdata('nama')){
-
-}else{
-    redirect(base_url('/login'));
-    echo $this->session->userdata('nama');
-}?>
+?>
 <script src="<?php echo base_url()?>assets/js/uploadcsv.js"></script>
 <body>
       <div class="col-md-9">
@@ -18,12 +8,12 @@ if($this->session->userdata('nama')){
               <center><h1>UPLOAD FILE ADMIN</h1></center>
               <br></br>
 
-          <div class="card" style="margin-left: 250px; margin-right: 250px; border-style: solid; padding: 15px;">
+          <div class="card" style="margin-left: 250px; margin-right: 250px; border-style: solid; padding: 25px;">
                 <form method="post" id="import_csv" enctype="multipart/form-data">
                     <div class="form-group">
                         <label>Select CSV File</label>
                         <input type="file" name="csv_file" id="csv_file" required accept=".csv" />
-                            <br /><div class="hidden" id="imported_csv_data"></div>
+                        <br>
                         <input type="submit" class="btn btn-warning" value="upload" id="import_csv_btn" />
                     </div>
                 </form>
