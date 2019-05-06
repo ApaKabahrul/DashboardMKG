43f00fd6451f1dfe3f4631a8493e0f2c0555db64
Dean Christoper <37867001+DChristoper@users.noreply.github.com>
2019-05-06 12:35:02 +0700

    Add files via upload

---
+++ SourceCode/assets/js/tutorial.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.satu').removeClass('active');
+    $('.dua').removeClass('active');
+    $('.tiga').removeClass('active');
+    $('.empat').removeClass('active');
+    $('.lima').removeClass('active');
+    $('.enam').removeClass('active');
+    $('.tujuh').removeClass('active');
+    $('.delapan').removeClass('active');
+    $('.sembilan').addClass('active');
+
+});
